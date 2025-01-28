2c9940192f4a709022262c0674a9bdabb78dfe1f
YanGraim <yangraim@gmail.com>
2025-01-28 11:45:42 -0300

    feat: cria rotas das pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Home } from "./pages/home";
+import { Admin } from "./pages/admin";
+import { Login } from "./pages/login";
+import { Networks } from "./pages/networks";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/admin",
+    element: <Admin/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/admin/social",
+    element: <Networks/>
+  }
+])
 
 
-function App() {
-
-  return (
-    <div>
-      <h1>teste</h1>
-    </div>
-  ) 
-}
-
-export default App
+export { router }
