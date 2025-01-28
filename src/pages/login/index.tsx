38b88409b05f3d497ded3e25171ce685393d23f2
YanGraim <yangraim@gmail.com>
2025-01-28 14:41:44 -0300

    feat: cria estrutura da page de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 
 
 
 export function Login() {
     return (
-        <div>
-            <h1>LOGIN</h1>
+        <div className="w-full h-screen flex flex-col items-center justify-center">
+            <Link to={"/"}>
+                <h1 className="mt-11 text-white mb-7 font-bold text-5xl">Dev
+                <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
+                </h1>
+            </Link>
         </div>
     )
 }
