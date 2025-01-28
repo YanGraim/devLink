869d6be9c61be49b8637cf605d091fffa3ca63ae
YanGraim <yangraim@gmail.com>
2025-01-28 11:38:09 -0300

    add pages

---
+++ src/networks/index.tsx
@@ -0,0 +1,9 @@
+
+
+export function Networks() {
+    return (
+        <div>
+            <h1>PAgina Network</h1>
+        </div>
+    )
+}
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,10 @@
+
+
+
+export function Admin() {
+    return (
+        <div>
+            <h1>Page ADMIN</h1>
+        </div>
+    )
+}
