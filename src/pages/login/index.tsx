4d3f1ed4988a5214f7e12b3dc9e60743851cb02a
YanGraim <yangraim@gmail.com>
2025-01-28 15:04:27 -0300

    feat: cria componente input para ser utilizado na page de login, passando props

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { useState } from "react";
 
 
 
 export function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
     return (
         <div className="w-full h-screen flex flex-col items-center justify-center">
             <Link to={"/"}>
@@ -10,6 +15,17 @@ export function Login() {
                 <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
                 </h1>
             </Link>
+
+            
+            <form className="w-full max-w-xl flex flex-col px-2">
+                <Input placeholder="Digite seu email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <Input placeholder="**********" type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+                <button 
+                 type="submit" 
+                 className="h-9 bg-blue-600 text-white rounded border-0 text-lg font-medium">
+                    Acessar
+                </button>
+            </form>
         </div>
     )
 }
