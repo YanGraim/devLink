bfd4fa10b8f318504b91b11f72ab8d21dd45afff
YanGraim <yangraim@gmail.com>
2025-01-29 11:00:42 -0300

    feat: função logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,17 @@
 import { BiLogOut } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
+import { auth } from "../../services/firebaseConnection";
+import { signOut } from "firebase/auth";
+
 
 export function Header() {
+
+    async function handleLogout() {
+        await signOut(auth);
+    }
+
+
     return (
         <header className="w-full max-w-2xl mt-4 px-2">
             <nav className="w-full bg-white h-12 flex items-center justify-between rounded-md px-3">
@@ -18,7 +27,7 @@ export function Header() {
                     </Link>
                 </div>
 
-                <button>
+                <button onClick={handleLogout}>
                     <BiLogOut size={28} color="#db2629"></BiLogOut>
                 </button>
             </nav>
