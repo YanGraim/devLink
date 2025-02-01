1ad15b8bbb3dc5a4e594517391e84e58bc01e649
YanGraim <yangraim@gmail.com>
2025-01-31 23:17:06 -0300

    style: estiliza pagina de error

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 export function ErrorPage() {
     return (
-        <div>
-            <h1>Página não encontrada</h1>
-            <p>Você caiu em uma página que não existe!</p>
+        <div className="w-full min-h-screen flex flex-col items-center justify-center text-white">
+            <h1 className="text-6xl font-bold mb-2">404</h1>
+            <h1 className="text-4xl font-bold mb-4">Página não encontrada</h1>
+            <p className="italic text-xl mb-4">Você caiu em uma página que não existe!</p>
 
-            <Link to={"/"}>Voltar para home</Link>
+            <Link className="bg-gray-50/20 py-1 px-4 rounded-md" to={"/"}>Voltar para home</Link>
         </div>
     )
 }
